e0c7d04f4fd2e4be69043f8fb02a4a957bc63472
seinlei <seimleihtarni@gmail.com>
2024-04-26 17:30:36 +0630

    Added Comments and Update Aboutus

--- scripts/script2.js
+++ scripts/script2.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function()
 {
+    // select elements from DOM 
     const joinButton = document.querySelector('.join');
     const nicknameInput = document.querySelector('#playername');
     const welcomeMessage = document.querySelector('.message');
@@ -7,19 +8,22 @@ document.addEventListener('DOMContentLoaded', function()
     const nicknameForm = document.querySelector('.container');
 
 
+// even listen when join button is clicked 
+
 joinButton.addEventListener('click', function()
 {
-    const nickname = nicknameInput.value.trim();
+    const nickname = nicknameInput.value.trim(); // get the trim value of the nickname input to avoid blanks 
 
+    // check if the nickname is empty
     if (nickname !== '')
     {
-        nicknameDisplay.textContent= ' " ' + nickname + ' " ';
-        welcomeMessage.style.display = 'block';
-        nicknameForm.style.display = 'none'; 
+        nicknameDisplay.textContent= ' " ' + nickname + ' " '; // display the nickname in nickname display box 
+        welcomeMessage.style.display = 'block'; // display welcome message 
+        nicknameForm.style.display = 'none'; // hide the nickname form 
 
         setTimeout(function()
         {
-            window.location.href= 'game.html';
+            window.location.href= 'game.html'; // go to game.html after 10 secs 
         },
         10000);
 
@@ -29,47 +33,58 @@ joinButton.addEventListener('click', function()
 
 });
 
+// function to play the sound and show instructions when clicked the start button 
+
 function playSound()
 {
-    var audio = document.getElementById("clicksound");
-    audio.play();
+    var audio = document.getElementById("clicksound"); // get the audio element by ID
+    audio.play(); // paly the audio 
 
-    var instructionsDiv = document.querySelector('.instructions');
-    instructionsDiv.style.display = 'block';
+    var instructionsDiv = document.querySelector('.instructions'); // select the instructions 
+    instructionsDiv.style.display = 'block'; // display instructions 
     
 
-    setTimeout (function()
-    {
-    instructionsDiv.style.display = 'none';
-    window.location.href= "game2.html";
-    },10000);
+    // time out function to hide the instructions and go the game2.html after 10 seconds 
+
+                setTimeout (function()
+                {
+
+                instructionsDiv.style.display = 'none';
+                window.location.href= "game2.html";
+
+                },10000);
 
 };
 
-document.addEventListener("DOMContentLoaded", function() {
-    const slides = document.querySelectorAll('.slide');
-    const texts = document.querySelectorAll('.slide-overlay');
-    let currentSlide = 0;
-  
-    function showSlide(n) {
-        slides.forEach((slide, index) => {
-          if (index === n) {
-            slide.style.display = "block"; 
-            texts[index].style.display = "block";
+document.addEventListener("DOMContentLoaded", function() 
+{
+    // select all elements class named slide
+  const slides = document.querySelectorAll('.slide');
+  let currentSlide = 0;
+
+  // function to show the slide 
+
+  function showSlide(n) 
+  {
+      slides.forEach((slide, index) => 
+      {
+          // If the index matches the current slide, display it, else hide it 
+          if (index === n) 
+          {
+              slide.style.display = "block";
           } else {
-            slide.style.display = "none"; 
-            texts[index].style.display = "none";
+              slide.style.display = "none";
           }
-        });
-      }
-  
-    function nextSlide() {
-      currentSlide = (currentSlide + 1) % slides.length;
-      showSlide(currentSlide);
-    }
-  
-    setInterval(nextSlide, 3000);
-  
-    showSlide(currentSlide);
-  });
-  
+      });
+  }
+
+  // function to display another slide 
+  function nextSlide() {
+      currentSlide = (currentSlide + 1) % slides.length; // increment current slide value
+      showSlide(currentSlide); // show the nextslide 
+  }
+
+  setInterval(nextSlide, 3000); // show the next slide after every 3 seconds 
+
+  showSlide(currentSlide); // first,show the first slide 
+});
